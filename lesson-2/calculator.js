5459deb77873cc65955b9b0a95a966c9f3fb7fc6
sebball <sebby00@gmail.com>
2022-11-27 03:01:46 +0100

    Calcator app with added ability to exit, play new games, reuse numbers

--- lesson-2/calculator.js
+++ lesson-2/calculator.js
@@ -19,52 +19,96 @@ function invalidNumber(number) {
 }
 
 
+function getNumber(isFirst) {
+  /* Asks the user to input the values they want calculated.
+  Validates the input.
+  Returns the response in array
+  */
+  let number;
+  if (isFirst) {
+    prompt("What is the first number?");
+    number = readline.question();
+  } else {
+    prompt("What is the other number?");
+    number = readline.question();
+  }
+
+  while (invalidNumber(number)) {
+    prompt("Hmm... that doesn't look like a valid number.");
+    number = readline.question();
+  }
+  return number;
+}
+
+function getOperator () {
+  /* Asks user which operator to use. Validates input.
+  Returns Number between 1 and 5.
+  */
+  prompt("What operation would you like to perform?\n1) Add 2)Subtract 3)Multiply 4)1st / 2nd 5)2nd / 1st");
+  let operation = readline.question();
+
+  while (!['1', '2', '3', '4', '5'].includes(operation)) {
+    prompt("Must choose 1, 2, 3, 4 or 5");
+    operation = readline.question();
+  }
+  return operation;
+}
+
+// eslint-disable-next-line max-lines-per-function
+function performCalculation(number1, number2, operation) {
+  /* Takes two numbers as operands. Input for operation
+  to perform expected as Number as follows:
+  1-addition, 2- subtraction, 3- multiplication, 4- division
+  Returns the the result of this calculation as Number.
+  */
+  let output;
+  switch (operation) {
+    case '1':
+      output = Number(number1) + Number(number2);
+      break;
+    case '2':
+      output = Number(number1) - Number(number2);
+      break;
+    case '3':
+      output = Number(number1) * Number(number2);
+      break;
+    case '4':
+      output = Number(number1) / Number(number2);
+      break;
+    case '5':
+      output = Number(number2) / Number(number1);
+      break;
+  }
+  prompt(`The result is ${output}`);
+  return output;
+}
+
+function runCalculator (reuseNumber = false, previousNum = NaN) {
+  let number1;
+  let number2;
+  if (reuseNumber) {
+    number1 = previousNum;
+  } else {
+    number1 = getNumber(true);
+  }
+  number2 = getNumber(false);
+
+  let operation = getOperator();
+
+  let output = performCalculation(number1, number2, operation);
+  prompt(`Would you like to: \n1)Perform a calculation on ${output}\n`
+  + `2)Perform a new calculation\n3)Exit`);
+
+  let userAnswer = readline.question();
+
+  if (userAnswer === "1") {
+    runCalculator(true, output);
+  } else if (userAnswer === "2") {
+    runCalculator();
+  }
+}
+
 prompt("Welcome to the calculator!");
 
-prompt("What is the first number?");
-let number1 = readline.question();
+runCalculator();
 
-
-while (invalidNumber(number1)) {
-  prompt("Hmm... that doesn't look like a valid number.");
-  number1 = readline.question();
-}
-
-
-prompt("What is the second number?");
-let number2 = readline.question();
-
-
-while (invalidNumber(number2)) {
-  prompt("Hmm... that doesn't look like a valid number.");
-  number2 = readline.question();
-}
-
-
-prompt("What operation would you like to perform?\n1) Add 2)Subtract 3)Multiply 4)Divide");
-let operation = readline.question();
-
-
-while (!['1', '2', '3', '4'].includes(operation)) {
-  prompt("Must choose 1, 2, 3 or 4");
-  operation = readline.question();
-}
-
-let output;
-
-switch (operation) {
-  case '1':
-    output = Number(number1) + Number(number2);
-    break;
-  case '2':
-    output = Number(number1) - Number(number2);
-    break;
-  case '3':
-    output = Number(number1) * Number(number2);
-    break;
-  case '4':
-    output = Number(number1) / Number(number2);
-    break;
-}
-
-prompt(`The result is ${output}`);
